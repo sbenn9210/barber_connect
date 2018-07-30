f6e31afab0b2e865edf434f42964ec14282e4426
Shawn Benny <shawn@Shawns-MBP.localdomain>
2018-07-30 16:39:06 -0500

    adding jwt authentication and validation

--- server.js
+++ server.js
@@ -1,33 +1,39 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
 
 const users = require("./routes/api/users");
 const profiles = require("./routes/api/profiles");
 const posts = require("./routes/api/posts");
-
+const keys = require("./config/keys");
 const app = express();
 
-//DB config
-const db = require("./config/keys").mongoURL;
+//body parser middlware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
+//DB config
+const db = keys.mongoURI;
 // Connect to MongoDB
 mongoose
   .connect(
-    process.env.db,
-
-    {
-      useNewUrlParser: true,
-      auth: {
-        user: process.env.shawnbenny1,
-        password: process.env.shawnbenny1
-      }
-    }
+    db,
+    { useNewUrlParser: true }
   )
-
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("hello"));
+//Passport middleware
+app.use(passport.initialize());
+
+//Passport Config
+require("./config/passport")(passport);
+
+//Use routes
+app.use("/api/users", users);
+app.use("/api/profiles", profiles);
+app.use("/api/posts", posts);
 
 //process.env.port is needed when deploying to heroku
 const port = process.env.PORT || 5000;
